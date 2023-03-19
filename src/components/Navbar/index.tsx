755201dbf9b5616784e5a2f2d281e6f2d60db1c3
Davi Patricio <davipatricio2006@gmail.com>
2023-03-19 12:12:03 +0000

    add navbar links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import Logo from '../Logo';
 import { NavbarContainer } from './styles';
 
@@ -7,6 +8,17 @@ export default function Navbar() {
   return (
     <NavbarContainer>
       <Logo />
+
+      <nav>
+        <Link href="/">Início</Link>
+        <Link href="/posts">Publicações</Link>
+        <Link
+          href="/auth/login"
+          style={{ marginLeft: 'auto', fontWeight: 'bold' }}
+        >
+          Autenticar-se
+        </Link>
+      </nav>
     </NavbarContainer>
   );
 }
--- src/components/Navbar/styles.tsx
+++ src/components/Navbar/styles.tsx
@@ -3,10 +3,24 @@ import styled from "styled-components";
 export const NavbarContainer = styled.header`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 15px 30px;
   background-color: #343a40;
 
+  width: 100%;
+
   * {
     color: #fff;
+  }
+
+  nav {
+    display: flex;
+    gap: 10px;
+    a {
+      text-decoration: none;
+      &:hover {
+        color: #ccc;
+      }
+    }
   }
 `;
