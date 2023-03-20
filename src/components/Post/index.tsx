36563c10d0bd2e751a88a0e13d174522c8ead0b2
Davi Patricio <davipatricio2006@gmail.com>
2023-03-20 00:53:06 +0000

    add skeleton

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import ContentLoader from 'react-content-loader';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { FeedPostFooter } from '../Feed/FeedPost/styles';
 import {
@@ -21,6 +23,31 @@ export type PostProps = {
   url: string;
 };
 
+const Loader = (
+  <ContentLoader
+    backgroundColor="#7e7e7ec5"
+    foregroundColor="#d1d1d1b0"
+    speed={2}
+    style={{ borderRadius: '5px', height: '40px', width: '100%' }}
+  >
+    <rect height="40px" rx="0" ry="0" width="100%" />
+  </ContentLoader>
+);
+
+const Article = (
+  <ContentLoader
+  backgroundColor="#7e7e7ec5"
+  foregroundColor="#d1d1d1b0"
+    style={{ borderRadius: '5px', height: '360px', width: '100%' }}
+  >
+    <rect height="20" rx="4" ry="4" width="100%" x="0" y="13" />
+    <rect height="20" rx="4" ry="4" width="70%" x="0" y="45" />
+    <rect height="20" rx="4" ry="4" width="100%" x="0" y="77" />
+    <rect height="20" rx="4" ry="4" width="100%" x="0" y="109" />
+    <rect height="20" rx="4" ry="4" width="40%" x="0" y="141" />
+  </ContentLoader>
+);
+
 export default function Post({
   author,
   comments,
@@ -30,10 +57,18 @@ export default function Post({
   upvotes,
   url,
 }: PostProps) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2_000);
+  }, []);
+
   return (
     <PostContainer>
       <PostHeader>
-        <h1>{title}</h1>
+        <h1>{loaded ? title : Loader}</h1>
 
         <FeedPostFooter>
           <span>{upvotes} votos</span>
@@ -52,13 +87,14 @@ export default function Post({
       </PostHeader>
 
       <PostContent>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+        {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
         praesentium iste porro cumque quo asperiores debitis eligendi modi
         placeat autem, necessitatibus maxime voluptates laboriosam voluptatibus
         dolorem dignissimos distinctio doloribus recusandae! Lorem, ipsum dolor
         sit amet consectetur adipisicing elit. Amet dolores quis cumque autem
         quasi, voluptas facilis in quos animi atque minima deleniti consequuntur
-        impedit iste nihil illum fugit sed reiciendis.
+        impedit iste nihil illum fugit sed reiciendis. */}
+        {Article}
       </PostContent>
 
       <PostSocial>
